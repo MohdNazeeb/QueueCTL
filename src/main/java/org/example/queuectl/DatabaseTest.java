25516bbed6afb90a35935c409358b1c32c8dc5ea
Mohd Nazeeb Mansoori <warsinazeeb@gmail.com>
2026-07-16 20:30:24 +0530

    feat(db): initialize SQLite database and jobs table

---
+++ src/main/java/org/example/queuectl/DatabaseTest.java
@@ -0,0 +1,12 @@
+package org.example.queuectl;
+
+import org.example.queuectl.config.DatabaseInitializer;
+
+public class DatabaseTest {
+
+    public static void main(String[] args) {
+
+        DatabaseInitializer.initialize();
+
+    }
+}
---
+++ src/main/java/org/example/queuectl/config/DatabaseInitializer.java
@@ -0,0 +1,37 @@
+package org.example.queuectl.config;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DatabaseInitializer {
+
+    public static void initialize() {
+
+        String sql = """
+                CREATE TABLE IF NOT EXISTS jobs (
+                    id TEXT PRIMARY KEY,
+                    command TEXT NOT NULL,
+                    state TEXT NOT NULL,
+                    attempts INTEGER NOT NULL,
+                    max_retries INTEGER NOT NULL,
+                    created_at TEXT NOT NULL,
+                    updated_at TEXT NOT NULL
+                );
+                """;
+
+        try (
+                Connection connection = DatabaseConfig.getConnection();
+                Statement statement = connection.createStatement()
+        ) {
+
+            statement.execute(sql);
+
+            System.out.println("Jobs table created successfully!");
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+}
